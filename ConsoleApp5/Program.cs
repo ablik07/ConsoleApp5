2d7d94048a24ddbd7989858c672bcc9c1e17b22f
ablik07 <karolina.koptu@bk.ru>
2025-10-19 23:00:23 +0300

    Создание классов для брони оружия и лечебного зелья

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public virtual void Use(Player player) { }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+public class HealthPotion : Item
+{
+    public HealthPotion()
+    {
+        Name = "Лечебное зелье";
+    }
+
+    public override void Use(Player player)
+    {
+        player.HP = 100;
+        Console.WriteLine("Здоровье восстановлено!");
+    }
+}
+
+public class Weapon : Item
+{
+    public int Damage { get; set; }
+    public int Attack { get; set; }
+
+    public Weapon(string name, int damage, int attack)
+    {
+        Name = name;
+        Damage = damage;
+        Attack = attack;
+    }
+
+    public override void Use(Player player)
+    {
+        player.CurrentWeapon = this;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} (Урон: {Damage}, Атака: {Attack})";
+    }
+}
+
+public class Armor : Item
+{
+    public int Defense { get; set; }
+
+    public Armor(string name, int defense, int protection)
+    {
+        Name = name;
+        Defense = defense;
+    }
+
+    public override void Use(Player player)
+    {
+        player.CurrentArmor = this;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} (Защита: {Defense})";
+    }
+}
+
