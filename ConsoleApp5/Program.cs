85cf10d71277155ef35c9be16cc3528a16bb8d85
ablik07 <karolina.koptu@bk.ru>
2025-10-19 23:00:57 +0300

    Игрок

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -71,3 +71,49 @@ public class Armor : Item
     }
 }
 
+public class Player
+{
+    public int HP { get; set; }
+    public bool IsFrozen { get; set; }
+    public Weapon CurrentWeapon { get; set; }
+    public Armor CurrentArmor { get; set; }
+
+    public int Attack => CurrentWeapon?.Attack ?? 5;
+    public int Defense => CurrentArmor?.Defense ?? 2;
+
+    public Player(int hp)
+    {
+        HP = hp;
+        CurrentWeapon = new Weapon("Кулаки", 5, 5);
+        CurrentArmor = new Armor("Одежда", 2, 2);
+    }
+
+    public bool TryDodge()
+    {
+        Random random = new Random();
+        return random.Next(100) < 40;
+    }
+
+    public int CalculateBlockedDamage(int damage)
+    {
+        Random random = new Random();
+        double blockPercent = random.Next(70, 101) / 100.0;
+        return Math.Max(0, damage - (int)(Defense * blockPercent));
+    }
+
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+    }
+
+    public string GetStatus()
+    {
+        return $"Здоровье: {HP}";
+    }
+
+    public string GetEquipment()
+    {
+        return $"Оружие: {CurrentWeapon?.Name}, Броня: {CurrentArmor?.Name}";
+    }
+}
+
